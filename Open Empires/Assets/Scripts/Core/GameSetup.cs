f693a4eb06ee92819316b1fa8fbdc3abda72c94b
coleshin27 <43325099+coleshin27@users.noreply.github.com>
2026-05-10 00:47:14 -0700

    10% smaller on resource drop offs

--- Open Empires/Assets/Scripts/Core/GameSetup.cs
+++ Open Empires/Assets/Scripts/Core/GameSetup.cs
@@ -2026,7 +2026,7 @@ namespace OpenEmpires
                     break;
                 case BuildingType.Mill:
                 {
-                    var spriteMill = CreateBuildingSpritePrefab("Mill", "EnglishMill", 2, 2, 6f, 1.54f / 6f);
+                    var spriteMill = CreateBuildingSpritePrefab("Mill", "EnglishMill", 2, 2, 5.4f, 1.54f / 6f);
                     if (spriteMill != null)
                     {
                         AddMillInfluenceZone(spriteMill, spriteMill.GetComponent<BuildingView>(), buildingData.PlayerId);
@@ -2039,11 +2039,11 @@ namespace OpenEmpires
                     break;
                 }
                 case BuildingType.LumberYard:
-                    prefab = CreateBuildingSpritePrefab("LumberYard", "LumberCamp", 2, 2, 6f, 1.54f / 6f)
+                    prefab = CreateBuildingSpritePrefab("LumberYard", "LumberCamp", 2, 2, 5.4f, 1.54f / 6f)
                           ?? CreateLumberYardPrefab(buildingData.PlayerId);
                     break;
                 case BuildingType.Mine:
-                    prefab = CreateBuildingSpritePrefab("Mine", "MiningCamp", 2, 2, 6f, 1.54f / 6f)
+                    prefab = CreateBuildingSpritePrefab("Mine", "MiningCamp", 2, 2, 5.4f, 1.54f / 6f)
                           ?? CreateMinePrefab(buildingData.PlayerId);
                     break;
                 case BuildingType.ArcheryRange:
